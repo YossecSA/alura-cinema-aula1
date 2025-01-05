d49a05d1254a1ec88661d6aade7f7b39efe6f22c
YossecSA <yosec.arratea22@gmail.com>
2025-01-05 02:36:47 -0500

    se modifico y mejoro el diseño

--- src/pages/favoritos/index.js
+++ src/pages/favoritos/index.js
@@ -1,10 +1,15 @@
-import sytles from './favoritos.module.css'
+import Banner from "components/Banner";
+import sytles from "./favoritos.module.css";
+import Titulo from "components/Titulo";
 
-function Favoritos(){
-return (
-    <>
-    a
-    </>
-)
+function Favoritos() {
+    return (
+        <>
+            <Banner img="favoritos" color="#154580"></Banner>
+            <Titulo>
+                <h1>Un lugar para guardar sus videos favoritos</h1>
+            </Titulo>
+        </>
+    );
 }
-export default Favoritos
+export default Favoritos;
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,24 @@
 import Favoritos from "pages/favoritos";
 import Cabecera from "components/Cabecera/Cabecera";
 import Pie from "components/Pie";
+import Container from "components/Container";
 
 const { default: Inicio } = require("pages/Inicio");
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
 
-function AppRoutes(){
-    return(
+function AppRoutes() {
+    return (
         <BrowserRouter>
-        <Cabecera></Cabecera>
-        <Routes>
-            <Route path="/" element={<Inicio/>}></Route>
-            <Route path="/favoritos" element={<Favoritos/>}></Route>
-        </Routes>
-        <Pie></Pie>
+            <Cabecera></Cabecera>
+            <Container>
+                <Routes>
+                    <Route path="/" element={<Inicio />}></Route>
+                    <Route path="/favoritos" element={<Favoritos />}></Route>
+                </Routes>
+            </Container>
+            <Pie></Pie>
         </BrowserRouter>
-    )
+    );
 }
 
 export default AppRoutes;
